1008d11be2f3534bda2d0a194aa4877959bfdb08
RomchikkF <r.fesenko@g.nsu.ru>
2023-10-02 12:37:21 +0700

    init

---
+++ Task_1_1_1/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main
+{
+    static void add_to_heap(ArrayList<Integer> heap, int x){
+        int i = heap.size();heap.add(x);
+        while (i > 0 && heap.get(i) < heap.get((i  - 1)/ 2)) {
+            Collections.swap(heap, i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    static int take_from_heap(ArrayList<Integer> heap){
+        int result = heap.get(0);
+        int n = heap.size() - 1;
+        heap.set(0, heap.get(n));
+        heap.remove(n);
+        int i = 0;
+        while (2 * i + 1 < n && (heap.get(i) > heap.get(2 * i + 1) || (2 * i + 2 < n && heap.get(i) < heap.get(2 * i + 2)))){
+            if (2 * i + 2 >= n || heap.get(i + 2) > heap.get(2 * i + 1)) {
+                Collections.swap(heap, i, 2 * i + 1);
+                i = 2 * i + 1;
+            } else {
+                Collections.swap(heap, i, 2 * i + 2);
+                i = 2 * i + 2;
+            }
+        }
+        return result;
+    }
+
+    static int[] heapsort(int[] list){
+        ArrayList<Integer> heap = new ArrayList<>();
+        for (int j : list) {
+            add_to_heap(heap, j);
+        }
+        int n = heap.size();
+        int[] sorted_list = new int[n];
+        for (int i = 0; i < n; ++i){
+            sorted_list[i] = take_from_heap(heap);
+        }
+        return sorted_list;
+    }
+    public static void main(String[] Args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] list = new int[n];
+        for (int i = 0; i < n; ++i){
+            list[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(heapsort(list)));
+    }
+}
