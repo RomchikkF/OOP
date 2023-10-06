a1d474844c66eb52187d23139dd92d17e4c69ab4
RomchikkF <r.fesenko@g.nsu.ru>
2023-10-06 10:28:42 +0700

    fix

--- Task_1_1_1/src/main/java/Main.java
+++ Task_1_1_1/src/main/java/Main.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main
 {
     static void add_to_heap(ArrayList<Integer> heap, int x){
-        int i = heap.size();heap.add(x);
-        while (i > 0 && heap.get(i) < heap.get((i  - 1)/ 2)) {
+        int i = heap.size();
+        heap.add(x);
+        while (i > 0 && heap.get(i) < heap.get((i  - 1) / 2)) {
             Collections.swap(heap, i, (i - 1) / 2);
             i = (i - 1) / 2;
         }
@@ -19,8 +19,8 @@ public class Main
         heap.set(0, heap.get(n));
         heap.remove(n);
         int i = 0;
-        while (2 * i + 1 < n && (heap.get(i) > heap.get(2 * i + 1) || (2 * i + 2 < n && heap.get(i) < heap.get(2 * i + 2)))){
-            if (2 * i + 2 >= n || heap.get(i + 2) > heap.get(2 * i + 1)) {
+        while ((2 * i + 1 < n && heap.get(i) > heap.get(2 * i + 1)) || (2 * i + 2 < n && heap.get(i) > heap.get(2 * i + 2))){
+            if (2 * i + 2 >= n || heap.get(2 * i + 2) > heap.get(2 * i + 1)) {
                 Collections.swap(heap, i, 2 * i + 1);
                 i = 2 * i + 1;
             } else {
@@ -45,12 +45,7 @@ public class Main
     }
     public static void main(String[] Args)
     {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] list = new int[n];
-        for (int i = 0; i < n; ++i){
-            list[i] = in.nextInt();
-        }
-        System.out.println(Arrays.toString(heapsort(list)));
+        System.out.println(Arrays.toString(heapsort(new int[] {5, 4, 3, 2, 1})));
+        System.out.println(Arrays.toString(heapsort(new int[] {5, 6, 7, 9, 8, 4, 10, 3, 2, 1})));
     }
 }
