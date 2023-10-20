255c36d5ca66d8de138c45a70701d64170c2322f
RomchikkF <r.fesenko@g.nsu.ru>
2023-10-20 10:02:50 +0700

    test cases

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -5,8 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void test() {
+    void test0() {
+        assertArrayEquals(new int[] {}, Main.heapsort(new int[] {}));
+    }
+    @Test
+    void test1() {
         assertArrayEquals(new int[] {1, 2, 3, 4, 5}, Main.heapsort(new int[] {5, 4, 3, 2, 1}));
+    }
+    @Test
+    void test2(){
         assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, Main.heapsort(new int[] {5, 6, 7, 9, 8, 4, 10, 3, 2, 1}));
     }
 }
