64934643648974bbda8e2e0c2f00880230635785
RomchikkF <r.fesenko@g.nsu.ru>
2023-10-21 12:25:45 +0700

    style

--- Task_1_1_2/src/main/java/Polynomial.java
+++ Task_1_1_2/src/main/java/Polynomial.java
@@ -6,14 +6,14 @@ import static java.lang.Integer.max;
 public class Polynomial {
     int[] coefficients;
     int length;
-    public Polynomial(int[] _coefficients){
+    public Polynomial(int[] _coefficients) {
         coefficients = _coefficients;
         length = coefficients.length;
     }
 
-    public Polynomial plus(Polynomial other){
+    public Polynomial plus(Polynomial other) {
         int[] newCoefficients = new int[max(length, other.length)];
-        for (int i = 0; i < newCoefficients.length; ++i){
+        for (int i = 0; i < newCoefficients.length; ++i) {
             if (i < length){
                 newCoefficients[i] += coefficients[i];
             }
@@ -34,21 +34,21 @@ public class Polynomial {
 
     public Polynomial times(Polynomial other) {
         int[] newCoefficients = new int[length + other.length - 1];
-        for (int i = 0 ; i < length; ++i){
-            for (int j = 0; j < other.length; ++j){
+        for (int i = 0 ; i < length; ++i) {
+            for (int j = 0; j < other.length; ++j) {
                 newCoefficients[i + j] += coefficients[i] * other.coefficients[j];
             }
         }
         return new Polynomial(newCoefficients);
     }
 
-    public Polynomial minus(Polynomial other){
+    public Polynomial minus(Polynomial other) {
         return this.plus(other.times(-1));
     }
 
-    public int evaluate(int x){
+    public int evaluate(int x) {
         int result = 0;
-        for (int i = 0; i < length; ++i){
+        for (int i = 0; i < length; ++i) {
             result += pow(x, i) * coefficients[i];
         }
         return result;
@@ -56,7 +56,7 @@ public class Polynomial {
 
     public Polynomial differentiate(int n) {
         int[] newCoefficients = new int[max(1, length - n)];
-        for (int i = 0; i < length - n; ++i){
+        for (int i = 0; i < length - n; ++i) {
             newCoefficients[i] += coefficients[i + n] * (i + n);
         }
         return new Polynomial(newCoefficients);
@@ -70,15 +70,15 @@ public class Polynomial {
         StringBuilder str = new StringBuilder();
         for (int i = length - 1; i >= 0; --i){
             if (coefficients[i] != 0){
-                if (!str.isEmpty()){
+                if (!str.isEmpty()) {
                     str.append(" + ");
                 }
                 if (coefficients[i] != 1 || i == 0) {
                     str.append(coefficients[i]);
                 }
-                if (i > 1){
+                if (i > 1) {
                     str.append("x^").append(i);
-                } else if (i > 0){
+                } else if (i > 0) {
                     str.append("x");
                 }
             }
--- Task_1_1_2/src/test/java/PolynomialTest.java
+++ Task_1_1_2/src/test/java/PolynomialTest.java
@@ -10,7 +10,7 @@ class PolynomialTest {
         Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
         assertEquals("7x^3 + 6x^2 + 19x + 6", p1.plus(p2.differentiate(1)).toString());
     }
-    
+
     @Test
     void test1() {
         Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
