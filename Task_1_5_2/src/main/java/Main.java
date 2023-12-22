cc74ee0e3378a06a5bf9aa88bef88751227c6e33
RomchikkF <r.fesenko@g.nsu.ru>
2023-12-22 23:57:29 +0700

    all

---
+++ Task_1_5_2/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws ParseException, IOException {
+        Options options = new Options();
+        Option add = new Option("add", "add note");
+        add.setArgs(2);
+        options.addOption(add);
+        Option rm = new Option("rm", "remove note");
+        rm.setArgs(1);
+        options.addOption(rm);
+        Option show = new Option("show","show notes");
+        options.addOption(show);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        Notebook notebook = new Notebook("notebook.json");
+
+        if (cmd.hasOption("add")) {
+            var arguments = cmd.getOptionValues("add");
+            notebook.add(arguments[0], arguments[1]);
+        } else if (cmd.hasOption("rm")) {
+            notebook.remove(cmd.getOptionValue("rm"));
+        } else if (cmd.hasOption("show")) {
+            String[] arguments = cmd.getArgs();
+            if (arguments.length == 0) {
+                notebook.show();
+            } else {
+                String[] keywords = Arrays.copyOfRange(arguments, 2, arguments.length);
+                keywords = (String[]) Arrays.stream(keywords).map(String::toLowerCase).toArray();
+                notebook.show(LocalDateTime.parse(arguments[0]), LocalDateTime.parse(arguments[1]), keywords);
+            }
+        }
+    }
+}
---
+++ Task_1_5_2/src/main/java/Notebook.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Notebook {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String filename;
+
+    /** Init new notebook that stores notes in json file with given name */
+    public Notebook(String filename) throws IOException {
+        this.filename = filename;
+        File file = new File(filename);
+        if (file.createNewFile()) {
+            List<Note> empty = new ArrayList<>();
+            objectMapper.writeValue(file, empty);
+        }
+    }
+
+    /** Clear file */
+    public void clear() throws IOException {
+        File file = new File(filename);
+        List<Note> empty = new ArrayList<>();
+        objectMapper.writeValue(file, empty);
+    }
+
+    /** Add new note to file
+     * if note with the same name already exists nothing happens. */
+    public void add(String name, String text) throws IOException {
+        Note newNote = new Note(name, text);
+        File file = new File(filename);
+        List<Note> notes = objectMapper.readValue(file, new TypeReference<List<Note>>(){});
+        for (Note note : notes) {
+            if (note.getName().equals(name)) {
+                return;
+            }
+        }
+        notes.add(newNote);
+        objectMapper.writeValue(file, notes);
+    }
+
+    /** Removes note with given name. If there are no such note nothing happens */
+    public void remove(String name) throws IOException {
+        File file = new File(filename);
+        List<Note> notes = objectMapper.readValue(file, new TypeReference<List<Note>>(){});
+        notes = notes.stream().filter(note -> !note.getName().equals(name)).toList();
+        objectMapper.writeValue(file, notes);
+    }
+
+    /** Writes all notes */
+    public void show() throws IOException {
+        List<Note> notes = get();
+        for (Note note : notes){
+            System.out.println(note);
+        }
+    }
+
+    /** Writes all notes that were created from timeL to timeR
+     * and have at least one keyword in a name. */
+    public void show(LocalDateTime timeL, LocalDateTime timeR, String[] keywords) throws IOException {
+        List<Note> notes = get(timeL, timeR, keywords);
+        for (Note note : notes) {
+            System.out.println(note);
+        }
+    }
+
+    /** Returns list of all notes */
+    public List<Note> get() throws IOException {
+        File file = new File(filename);
+        return objectMapper.readValue(file, new TypeReference<List<Note>>() {});
+    }
+
+    /** Returns list of all notes that were created from timeL to timeR
+     * and have at least one keyword in a name. */
+    public List<Note> get(LocalDateTime timeL, LocalDateTime timeR, String[] keywords) throws IOException {
+        File file = new File(filename);
+        List<Note> notes = objectMapper.readValue(file, new TypeReference<List<Note>>() {});
+        return notes.stream().filter(note -> noteCheck(note, timeL, timeR, keywords)).toList();
+    }
+
+    private boolean noteCheck(Note note, LocalDateTime timeL, LocalDateTime timeR, String[] keywords) {
+        if (!note.isInInterval(timeL, timeR)) {
+            return false;
+        }
+        for (String keyword : keywords) {
+            if (note.getName().contains(keyword)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
